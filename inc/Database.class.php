08148f3b92499c628bffd75a0a20c97c75246c65
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-04-14 21:19:48 -0300

    Added Database class

---
+++ inc/Database.class.php
@@ -0,0 +1,29 @@
+<?php
+class Database
+{
+    private static $dbName = '';
+    private static $dbHost = '';
+    private static $dbUsername = '';
+    private static $dbUserPassword = '';
+
+    private static $cont = null;
+
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+
+    public static function connect() {
+        if (null === self::$cont) {
+            try {
+                self::$cont =  new PDO('mysql:host='.self::$dbHost.'; dbname='.self::$dbName, self::$dbUsername, self::$dbUserPassword);
+            } catch(PDOException $e) {
+                die($e->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+
+    public static function disconnect() {
+        self::$cont = null;
+    }
+}
